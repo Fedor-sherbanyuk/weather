2b76612a0fb31d5aad35fb0a2190aec85583976a
Fedor Sherbanyuk <fedor.sherbanyuk1@gmail.com>
2023-04-02 22:59:10 +0300

    new api

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 document.querySelector('#formWeather').addEventListener('submit', getOpenWeatherMap);
-const weatherApi = "884b6d1113f443099de83658230204";
-
+const api = "f9db622d38b4c2f2686d22ca788d6573";
 async function getOpenWeatherMap(event) {
     const data = document.querySelector('#formWeather');
-    const api = "f9db622d38b4c2f2686d22ca788d6573";
+
     const weatherApi = "884b6d1113f443099de83658230204";
     const weatherStackApi = "04ee8da4f6676b93bd427bb179b0a8f8";
     event.preventDefault();
@@ -64,20 +63,18 @@ document.querySelector('.selectFilter').addEventListener('change', function (eve
         const target = event.target.selectedIndex;
         const city = listOptions[target].innerText;
 
-        async function infoArray() {
+      async   function infoArray() {
             const array = [];
-            await fetch(`http://api.weatherapi.com/v1/current.json?key=${weatherApi}&q=${city}&aqi=no`)
+          await   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${api}`)
                 .then((response) => response.json())
                 .then((info) => {
-                    array.push({key: 'Region:', info: info.location.region});
-                    array.push({key: 'How does the temperature feel:', info: info.current.feelslike_c});
-                    array.push({key: 'Wind gust:', info: info.current.gust_kph});
-                    array.push({key: 'Humidity:', info: info.current.humidity});//влажность
-                    array.push({key: 'Pressure millimeters:', info: info.current.pressure_in});//давление millimeters
-                    array.push({key: 'Pressure in millibars:', info: info.current.pressure_mb});//давление millibars
-                    array.push({key: 'Wind km per hour:', info: info.current.wind_kph});//ветер км в час
-                    array.push({key: 'Loudiness in percent:', info: info.current.cloud});//облачность в процентах
-                    array.push({key: 'Temperature:', info: info.current.temp_c});//temperature
+                    array.push({key: 'How does the temperature feel:', info: info.main.feels_like});
+                    array.push({key: 'Wind gust:', info: info.wind.gust});
+                    array.push({key: 'Humidity:', info: info.main.humidity});//влажность
+                    array.push({key: 'Pressure millimeters:', info: info.main.pressure});//давление millimeters
+                    array.push({key: 'Wind km per hour:', info: info.wind.speed});//ветер км в час
+                    array.push({key: 'Loudiness in percent:', info: info.clouds.all});//облачность в процентах
+                    array.push({key: 'Temperature:', info: info.main.temp});//temperature
                 });
             document.querySelector('h1').insertAdjacentHTML('beforebegin', `<div class="card pointer card-list mt-3" style="max-width: 90vw; background: linear-gradient(green,white,red);"><div class="card-header-title is-half is-4" style="font-size: 22px;">City Italia: ${city}</div></div>`);
             array.forEach(el => {
